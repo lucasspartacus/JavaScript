4a6453444b5c71797fe65368827f0a86744ff30d
Lucas Spartacus <50811095+lucasspartacus@users.noreply.github.com>
2024-04-16 08:57:28 -0300

    Add files via upload

--- D3Js/grafico.js
+++ D3Js/grafico.js
@@ -12,33 +12,49 @@ function grafico(parametros){
             .attr('height', alturaPlotagem)
             .attr('transform', 'translate(20,20)');
 
-            let fnX = d3.scaleLinear()
-                        .domain([0, parametros.dados.length])
-                        .range([0, larguraPlotagem]);
+            let fnX = d3.scaleBand()
+                        .domain(parametros.dados.map(d=>d.chave))
+                        .range([0, larguraPlotagem])
+                        .padding(0.1);
                          
             let fnY = d3.scaleLinear()
-                        .domain([0, d3.max(parametros.dados)])
+                        .domain([0, d3.max(parametros.dados.map(d=>d.valor))])
                         .range([alturaPlotagem, 0]);
+
+            let fnCores = d3.scaleLinear()
+            .domain([0, d3.max(parametros.dados.map(d=>d.valor))])
+            .range(['#edf8e9', '#005a32']);
+
+            let eixoX = d3.axisBottom(fnX);
+            plotagem.append('g')
+            .attr('transform' , 'translate(0,'+ alturaPlotagem+')')
+            .call(eixoX);
+
+            let eixoY = d3.axisLeft(fnY);
+            plotagem.append('g')
+            .call(eixoY);
+        
 
             plotagem.selectAll('.barra')
             .data(parametros.dados)
             .enter()
             .append('rect')
             .classed('barra', true)
-            .attr('x', (d,i) => fnX(i))
-            .attr('y', (d) => fnY(d))
-            .attr('width', fnX(1)*0.9)
-            .attr('height', d => alturaPlotagem - fnY(d));
+            .attr('x', (d) => fnX(d.chave))
+            .attr('y', (d) => fnY(d.valor))
+            .attr('width', fnX.bandwidth())
+            .attr('height', (d) => alturaPlotagem - fnY(d.valor))
+            .attr('fill', (d,i) => fnCores(d.valor));
 
             plotagem.selectAll('.rotulo')
             .data(parametros.dados)
             .enter()
             .append('text')
             .classed('rotulo', true)
-            .text((d)=>d)
-            .attr('x', (d,i) =>fnX(i))
-            .attr('dx', ()=>fnX(1)*0.5 )
-            .attr('y', (d) => fnY(d)) 
+            .text((d)=>d.valor)
+            .attr('x', (d) =>fnX(d.chave))
+            .attr('dx', fnX.bandwidth()*0.5 )
+            .attr('y', (d) => fnY(d.valor)) 
             .attr('dy', -5);
 
 }
